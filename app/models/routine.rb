0c8f260b5f81cb305b7cfcf275641a3f5f355eb7
Steven Brooks <brooks.stevena@gmail.com>
2013-07-21 21:05:18 -0400

    fixed changed to delete exercises in routine

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,17 +1,17 @@
 class Routine < ActiveRecord::Base
-  attr_accessible :name, :exercise_name
+  attr_accessible :name, :exercise_name, :exercises_attributes
 
   has_many :lifts
   has_many :exercises, through: :lifts
 
   accepts_nested_attributes_for :exercises, allow_destroy: true, :reject_if => lambda { |a| a[:name].blank? }
 
-  def exercise_name
-    exercises.try(:name) 
-  end
+  # def exercise_name
+  #   exercises.try(:name) 
+  # end
 
-  def exercise_name=(name)
-    self.exercises << Exercise.find_by_name(name) if name.present?
-  end
+  # def exercise_name=(name)
+  #   self.exercises << Exercise.find_by_name(name) if name.present?
+  # end
 
 end
