e53f34c46fc96df176a5c6d400942a91abbb9a91
Steven Brooks <brooks.stevena@gmail.com>
2013-08-08 13:07:07 -0400

    jon commit -> working but needs to be cleaner

---
+++ app/assets/javascripts/routines.js
@@ -0,0 +1,14 @@
+ google.load("visualization", "1", {packages:["corechart"]});
+  google.setOnLoadCallback(drawChart);
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable(graph);
+
+    var options = {
+      title: 'Your Workout',
+      hAxis: {title: 'Exercises'},
+      vAxis: {title: 'Total Weight(pounds)'}
+    };
+
+    var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+  }
