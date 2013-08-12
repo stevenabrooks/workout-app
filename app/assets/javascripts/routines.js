474941ce9cafe8bbf1f3aa37a6d5a31ba5901f1b
Steven Brooks <brooks.stevena@gmail.com>
2013-08-12 17:29:25 -0400

    some shitty stying

--- app/assets/javascripts/routines.js
+++ app/assets/javascripts/routines.js
@@ -14,3 +14,9 @@ function drawChart() {
   var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
   chart.draw(data, options);
 }
+
+// [["exercise", "set 1", "set 2", "set 3", "set 4", "set 5"], 
+// ["Hip Abductor Machine", 600, 700, 800, 900, 1000], 
+// ["Squats", 1350, 1850, 2250, 0, 0], 
+// ["DB RDLs", 400, 480, 560, 0, 0], 
+// ["Standing Calf Raises", 360, 720, 1080, 0, 0]] 
--- app/views/users/_googlescript.js
+++ app/views/users/_googlescript.js
@@ -1,31 +1,32 @@
 function doGet() {
   var data = Charts.newDataTable()
-      .addColumn(Charts.ColumnType.STRING, "Name")
-      .addColumn(Charts.ColumnType.STRING, "Gender")
-      .addColumn(Charts.ColumnType.NUMBER, "Exercise")
+      .addColumn(Charts.ColumnType.NUMBER, "Date")
+      .addColumn(Charts.ColumnType.DATE, "RealDate")
+      .addColumn(Charts.ColumnType.STRING, "Exercise")
+      .addColumn(Charts.ColumnType.NUMBER, "Age")
       .addColumn(Charts.ColumnType.NUMBER, "Donuts eaten")
-      .addRow(["Steven", "Dips", 12000, 5])
-      .addRow(["Michael", "Bench Press", 12, 5])
-      .addRow(["Elisa", "Pullups", 20, 7])
-      .addRow(["Robert", "Dips", 7, 3])
-      .addRow(["John", "Toe to Bar", 54, 2])
-      .addRow(["Jessica", "Squats", 22, 6])
-      .addRow(["Aaron", "Deadlift", 3, 1])
-      .addRow(["Margareth", "Dips", 42, 8])
-      .addRow(["Miranda", "Squats", 33, 6])
+      .addRow([20130101, new Date(2013,01,01), "Dips", 12000, 5])
+      .addRow([20130102, new Date(2013,01,02),"Bench Press", 12, 5])
+      .addRow([20130103, new Date(2013,01,03),"Pullups", 20, 7])
+      .addRow([20130104, new Date(2013,01,04),"Dips", 7, 3])
+      .addRow([20130105, new Date(2013,01,05),"Squats", 54, 2])
+      .addRow([20130106, new Date(2013,01,06),"Squats", 22, 6])
+      .addRow([20130107, new Date(2013,01,07),"Deadlift", 3, 1])
+      .addRow([20130108, new Date(2013,01,08),"Dips", 42, 8])
+      .addRow([20130109, new Date(2013,01,09),"Bench Press", 33, 6])
       .build();
 
-  var exerciseFilter = Charts.newNumberRangeFilter()
+  var dateFilter = Charts.newNumberRangeFilter()
+      .setFilterColumnLabel("Date")
+      .build(); 
+
+  var exerciseFilter = Charts.newCategoryFilter()
       .setFilterColumnLabel("Exercise")
       .build();
 
-  var genderFilter = Charts.newCategoryFilter()
-      .setFilterColumnLabel("Gender")
-      .build();
-
-  var pieChart = Charts.newPieChart()
+  var columnChart = Charts.newColumnChart()
       .setDataViewDefinition(Charts.newDataViewDefinition()
-                            .setColumns([0,3]))
+                            .setColumns([1,4]))
       .build();
 
   var tableChart = Charts.newTableChart()
@@ -33,17 +34,17 @@ function doGet() {
 
   var dashboard = Charts.newDashboardPanel()
       .setDataTable(data)
-      .bind([exerciseFilter, genderFilter], [pieChart, tableChart])
+      .bind([dateFilter, exerciseFilter], [columnChart, tableChart])
       .build();
 
   var uiApp = UiApp.createApplication();
 
   dashboard.add(uiApp.createVerticalPanel()
                 .add(uiApp.createHorizontalPanel()
-                    .add(exerciseFilter).add(genderFilter)
+                    .add(dateFilter).add(exerciseFilter)
                     .setSpacing(70))
                 .add(uiApp.createHorizontalPanel()
-                    .add(pieChart).add(tableChart)
+                    .add(columnChart).add(tableChart)
                     .setSpacing(10)));
 
   uiApp.add(dashboard);
