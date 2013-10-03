06942e8d3e600924bc24f6ace0c4d5181fefff9d
Steven Brooks <brooks.stevena@gmail.com>
2013-10-03 14:33:49 -0400

    took out some crap

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ Workout::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#new'
+  root :to => 'routines#index'
 
   # See how all your routes lay out with "rake routes"
 
