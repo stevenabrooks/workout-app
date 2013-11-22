fe47c6a2423e6052d5734fb2fa2081af7bd13aa5
Steven Brooks <brooks.stevena@gmail.com>
2013-11-22 13:04:11 -0500

    cut the queries in half

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   def show
-    @user = current_user
+    @user = User.includes(:routines => {:lifts => [:exercise, :infos]}).find(current_user.id)
     @total_graph = @user.graph_for_total_weight_over_time(1)
   end
 end
